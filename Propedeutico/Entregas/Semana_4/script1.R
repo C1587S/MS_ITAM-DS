0f0e026c8d4bd0bfb5f4f6abf680b3d6ef4ea071
SC187 <11048067+C1587S@users.noreply.github.com>
2019-07-17 16:24:30 -0500

    Add files via upload

---
+++ Propedeutico/Entregas/Semana_4/script1.R
@@ -0,0 +1,32 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "x <- rpois(20, lambda = .61) \n",
+    "y <- rpois(20, lambda = .10)\n",
+    "plot(x, y, pch=5, col='red') \n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "3.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
